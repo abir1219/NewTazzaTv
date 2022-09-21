28fa85b21cc8d008a0b597031f46083a37be1224
abirchanda <jbmmatrix.150@gmail.com>
2022-09-21 10:08:32 +0530

    commit

--- lib/Screens/LiveVideo/LiveVideo.dart
+++ lib/Screens/LiveVideo/LiveVideo.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:new_tazza_tv_flutter/Widgets/MyAppBar.dart';
 import 'package:new_tazza_tv_flutter/Widgets/MyCustomDrawer.dart';
 import 'package:video_player/video_player.dart';
 import 'package:chewie/chewie.dart';
+import 'package:webview_flutter_plus/webview_flutter_plus.dart';
 
 class LiveVideo extends StatefulWidget {
   const LiveVideo({Key? key}) : super(key: key);
@@ -12,18 +14,18 @@ class LiveVideo extends StatefulWidget {
 }
 
 class _LiveVideoState extends State<LiveVideo> {
-  late VideoPlayerController controller;
+  //late VideoPlayerController controller;
 
   //await controller.initialize();
 
   @override
   void initState() {
-    loadVideoPlayer();
+    //loadVideoPlayer();
     super.initState();
   }
 
 
-  loadVideoPlayer(){
+  /*loadVideoPlayer(){
     controller = VideoPlayerController.network('https://bradmax.com/client/embed-player/ae5100d161f01d3871ca6e9a729b7e1a45977967_7821?mediaUrl=https://stream.we2live.in/taaza/taaza/playlist.m3u8');
     controller.addListener(() {
       setState(() {});
@@ -32,18 +34,18 @@ class _LiveVideoState extends State<LiveVideo> {
       setState(() {});
     });
 
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
     final GlobalKey<ScaffoldState> _key = GlobalKey();
     final size = MediaQuery.of(context).size;
 
-    final chewieController = ChewieController(
+    /*final chewieController = ChewieController(
       videoPlayerController: controller,
       autoPlay: true,
       looping: true,
-    );
+    );*/
 
     return SafeArea(
       child: Scaffold(
@@ -52,10 +54,15 @@ class _LiveVideoState extends State<LiveVideo> {
         appBar: MyAppBar(key_state: _key),
         body: Center(
           child: SizedBox(
-            height: size.height *.5,
-            child: Chewie(
-            controller: chewieController,
+            //height: size.height *.5,
+            child: WebViewPlus(
+              javascriptMode: JavascriptMode.unrestricted,
+              initialUrl: "https://rtsp.me/embed/KERz9sfK/",
             ),
+
+            /*Chewie(
+            controller: chewieController,
+            ),*/
           ),
         ),
       ),
